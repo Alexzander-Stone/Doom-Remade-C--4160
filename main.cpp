d2af8f957b9c67552840c9e7d403e02587873d6a
Alexzander William Stone <alexzas@ada15.cs.clemson.edu>
2018-02-23 19:28:57 -0500

    Reset intial starting point, too much bloat.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,9 @@
-// Brian Malloy        Data-Driven Object oriented Game Construction
-#include "engine.h"
+#include <iostream>
+#include <string>
 
-ImageFactory*  ImageFactory::instance = NULL;
-RenderContext* RenderContext::instance = NULL;
+int main(int, char*[])
+{
 
-int main(int, char*[]) {
-   try {
-      Engine engine;
-      engine.play();
-      delete RenderContext::getInstance();
-   }
-   catch (const string& msg) { std::cout << msg << std::endl; }
-   catch (...) {
-      std::cout << "Oops, someone threw an exception!" << std::endl;
-   }
-   return 0;
+
+    return 0;
 }
